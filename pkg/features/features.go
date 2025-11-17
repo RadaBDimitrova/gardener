92dc47529309fee0ac60b7f01d0c37118c7f23ab
Rada Dimitrova <rada.dimitrova@sap.com>
2025-11-17 16:24:44 +0200

    Introduce `PVCAutoscaler` feature gate

--- pkg/features/features.go
+++ pkg/features/features.go
@@ -74,6 +74,11 @@ const (
 	// owner: @hown3d
 	// alpha: v1.130.0
 	UseUnifiedHTTPProxyPort featuregate.Feature = "UseUnifiedHTTPProxyPort"
+
+	// PVCAutoscaler enables the autoscaling of persistent volumes.
+	// owner: @Kostov6 @plkokanov @RadaBDimitrova
+	// alpha: v1.138.0
+	PVCAutoscaler featuregate.Feature = "PVCAutoscaler"
 )
 
 // DefaultFeatureGate is the central feature gate map used by all gardener components.
@@ -110,6 +115,7 @@ var AllFeatureGates = map[featuregate.Feature]featuregate.FeatureSpec{
 	DoNotCopyBackupCredentials: {Default: true, PreRelease: featuregate.Beta},
 	OpenTelemetryCollector:     {Default: false, PreRelease: featuregate.Alpha},
 	UseUnifiedHTTPProxyPort:    {Default: false, PreRelease: featuregate.Alpha},
+	PVCAutoscaler:              {Default: false, PreRelease: featuregate.Alpha},
 }
 
 // GetFeatures returns a feature gate map with the respective specifications. Non-existing feature gates are ignored.

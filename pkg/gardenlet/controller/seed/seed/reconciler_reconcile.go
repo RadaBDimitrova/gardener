e90da8cee9cf6aa03d043ab05b0378ac45182dbc
Rada Dimitrova <rada.dimitrova@sap.com>
2025-11-18 09:34:53 +0200

    Add "Deploying PVC Autoscaler" task in seed reconciler

--- pkg/gardenlet/controller/seed/seed/reconciler_reconcile.go
+++ pkg/gardenlet/controller/seed/seed/reconciler_reconcile.go
@@ -419,6 +419,11 @@ func (r *Reconciler) runReconcileSeedFlow(
 			SkipIf:       seedIsGarden,
 		})
 		_ = g.Add(flow.Task{
+			Name:         "Deploying PVC autoscaler",
+			Fn:           c.pvcautoscaler.Deploy,
+			Dependencies: flow.NewTaskIDs(syncPointReadyForSystemComponents),
+		})
+		_ = g.Add(flow.Task{
 			Name:         "Deploying ETCD Druid",
 			Fn:           c.etcdDruid.Deploy,
 			Dependencies: flow.NewTaskIDs(syncPointReadyForSystemComponents),

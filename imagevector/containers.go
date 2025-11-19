d844dc1018469638c5ced8b2342ff6db50ef8807
Rada Dimitrova <rada.dimitrova@sap.com>
2025-11-19 12:44:25 +0200

    Update imagevector

--- imagevector/containers.go
+++ imagevector/containers.go
@@ -121,6 +121,8 @@ const (
 	ContainerImageNamePrometheus = "prometheus"
 	// ContainerImageNamePrometheusOperator is a constant for an image in the image vector with name 'prometheus-operator'.
 	ContainerImageNamePrometheusOperator = "prometheus-operator"
+	// ContainerImageNamePVCAutoscaler is a constant for an image in the image vector with name 'pvc-autoscaler'.
+	ContainerImageNamePVCAutoscaler = "pvc-autoscaler"
 	// ContainerImageNameTelegraf is a constant for an image in the image vector with name 'telegraf'.
 	ContainerImageNameTelegraf = "telegraf"
 	// ContainerImageNameTerminalControllerManager is a constant for an image in the image vector with name 'terminal-controller-manager'.
